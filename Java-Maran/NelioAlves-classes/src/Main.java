78ebb91b6b9e199b7b8b46afa964339fb5b233ec
Maran BR <maran.morimoto@mercadolivre.com>
2023-02-16 23:21:30 -0300

    version 0

--- Java-Maran/NelioAlves-classes/src/Main.java
+++ Java-Maran/NelioAlves-classes/src/Main.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        Circle circle = new Circle();
+
+        System.out.println("Enter radius: ");
+        circle.radius = sc.nextDouble();
+
+        circle.toString();
+
+        sc.close();
     }
 }
